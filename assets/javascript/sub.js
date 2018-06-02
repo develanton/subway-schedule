8f977a4361197384d372c6d55a4a0357d2d0a4dd
Develanton <develanton@gmail.com>
2018-06-01 23:47:20 -0500

    html finished

---
+++ assets/javascript/sub.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+console.log("Its alive");
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB7yFTL2TaTjB7qABq6EEY8tUhn_sqq-A4",
+    authDomain: "develanton-train-schedule.firebaseapp.com",
+    databaseURL: "https://develanton-train-schedule.firebaseio.com",
+    projectId: "develanton-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "431157652695"
+  };
+  firebase.initializeApp(config);
+
+})
